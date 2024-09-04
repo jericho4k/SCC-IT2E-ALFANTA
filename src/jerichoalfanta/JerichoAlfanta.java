ef7f95e3c223b2539ea45d94e6fa2d9b14f5e971
= <=>
2024-09-04 17:06:09 +0800

    update my repo balik

--- src/jerichoalfanta/JerichoAlfanta.java
+++ src/jerichoalfanta/JerichoAlfanta.java
@@ -1,21 +1,69 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package jerichoalfanta;
 
-/**
- *
- * @author SCC-COLLEGE
- */
+package jerichoalfanta;
+import java.util.Scanner;
+
 public class JerichoAlfanta {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+
+        Grades[] gr = new Grades[100];
+       
+        System.out.println("Enter number of students: ");
+        int ns = sc.nextInt();
+
+        int numPassed = 0;
+        int numFailed = 0;
+        double totalSum = 0.0;
+
+        for (int i = 0; i < ns; i++) {
+
+            gr[i] = new Grade();
+           
+            System.out.println("Enter details of student " + (i + 1));
+           
+            System.out.print("Id: ");
+            int id = sc.nextInt();
+           
+            System.out.print("Name: ");
+            String name = sc.next();
+           
+            System.out.print("Prelim: ");
+            double prelim = sc.nextDouble();
+           
+            System.out.print("Midterm: ");
+            double midterm = sc.nextDouble();
+           
+            System.out.print("Prefinal: ");
+            double prefinal = sc.nextDouble();
+           
+            System.out.print("Finals: ");
+            double finals = sc.nextDouble();
+
+            gr[i].addGrade(id, name, prelim, midterm, prefinal, finals);
+
+            double average = (prelim + midterm + prefinal + finals) / 4.0;
+            totalSum += average;
+           
+            if (average >= 3.0) {
+                numPassed++;
+            } else {
+                numFailed++;
+            }
+        }
+
+        System.out.printf("%-10s %-15s %-7s %-7s %-7s %-7s %-10s %-7s\n",
+                "Id", "Name", "Prelim", "Midterm", "Prefinal", "Finals", "Average", "Status");
+        for (int i = 0; i < ns; i++) {
+            gr[i].viewGrade();
+        }
+
+        double classAverage = totalSum / ns;
+        System.out.println("\nTotal number of students: " + ns);
+        System.out.printf("Total class average: %.2f\n", classAverage);
+        System.out.println("Number of students passed: " + numPassed);
+        System.out.println("Number of students failed: " + numFailed);
+
+        sc.close();
     }
-    
 }
